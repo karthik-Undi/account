733caeec982ff748b54a6215e8ef03aacefe4d04
karthik <karthik.venkata@kanini.com>
2021-02-17 12:08:10 +0530

    Add project files.

---
+++ account/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace account
+{
+    class Account
+    {
+        private int id;
+        private String accountType;
+        private Double balance;
+        public int ID   // property
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String AccountType   // property
+        {
+            get { return accountType; }
+            set { accountType = value; }
+        }
+        public Double Balance  // property
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        
+        public Account() { }
+        public Account(int id,String accountType,Double balance)
+        {
+            this.accountType = accountType;
+            this.id = id;
+            this.balance = balance;
+        }
+        public Boolean Withdraw(Double amt)
+        {
+
+            if (amt > balance) 
+            {
+                Console.WriteLine("Insufficient Funds");
+                return false;
+            }
+            else
+            {
+                balance = balance - amt;
+                return true;
+            }
+            
+        }
+        public Boolean Deposit(Double amt)
+        {
+            balance = balance + amt;
+            return true;
+        }
+
+        public  void GetDetails()
+        {
+            Console.WriteLine("Account ID : " + ID + "\nAccount Type : " + AccountType + "\nBalance : " + Balance);
+        }
+
+
+    }
+}
---
+++ account/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace account
+{
+    class Program
+    {
+        public static List<Account> AccountList = new List<Account>();
+
+        
+        public static void RegisterAccount()
+        {
+            int Aid;
+            String AccType="";
+            Double AccBal=0,DPAmt=0,WDAmt=0;
+            Console.WriteLine("Enter Your ID:");
+            Aid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose Account Type\n1.Savings\n2.Current\n3.Business");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    AccType = "Savings";
+                    break;
+                case 2:
+                    AccType = "Current";
+                    break;
+                case 3:
+                    AccType = "Business";
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice");
+                    break;
+            }
+
+            AccountList.Add(new Account(Aid, AccType, AccBal));
+            int index = AccountList.FindIndex(Acc => Acc.ID == Aid);
+
+            Console.WriteLine("Enter amount to Deposit: ");
+            DPAmt = Convert.ToDouble(Console.ReadLine());
+            AccountList[index].Deposit(DPAmt);
+            
+            Console.WriteLine("Enter amount to Withdraw: ");
+            WDAmt = Convert.ToDouble(Console.ReadLine());
+            AccountList[index].Withdraw(WDAmt);
+            AccountList[index].GetDetails();
+            Console.WriteLine("------------------------");
+
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; ; i++)
+            {
+                RegisterAccount();
+            }
+        }
+    }
+}
